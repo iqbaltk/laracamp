ed255570a15eeaeefdf2aadfacdfa13c7b62223a
iqbaltk <iqbaltri156@gmail.com>
2021-12-12 01:36:32 +0700

    install breeze

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,11 +7,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-use ILLuminate\Database\Eloquent\SoftDeletes;
+// use ILLuminate\Database\Eloquent\SoftDeletes;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable, SoftDeletes;
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
--- database/seeders/AdminUserSeeder.php
+++ database/seeders/AdminUserSeeder.php
@@ -18,7 +18,7 @@ class AdminUserSeeder extends Seeder
             'name' => 'admin',
             'email' => 'admin@laracamp.com',
             'email_verified_at' => date('Y-m-d H:i:s', time()),
-            'password'=> \bcrypt('password'),
+            'password' => \bcrypt('password'),
             'is_admin' => true,
         ]);
     }
